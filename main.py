1d5da695a44ca9f0904901f81e661cecf9d1fef9
robbiedv <robbie.c.devries@gmail.com>
2019-09-05 22:40:55 -0500

    reduced imports, first requests.get test

--- main.py
+++ main.py
@@ -1,9 +1,14 @@
 #! /bin/bash/python3
 
+import requests 
 from requests import get
 from requests.exceptions import RequestException
 from contextlib import closing
 from bs4 import BeautifulSoup
+
+stats = requests.get('http://www.nhl.com/stats/player?aggregate=1&reportType=season&seasonFrom=20142015&seasonTo=20182019&gameType=2&filter=gamesPlayed,gte,1&sort=points,goals,assists')
+
+print(stats.text)
 
 #ultimate goal is to have a list of players with
 #a high probability of being drafted that will 
