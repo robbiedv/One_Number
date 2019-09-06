f04aeeba684188cf9abbe86dab22e8b1055a5136
robbiedv <robbie.c.devries@gmail.com>
2019-09-05 22:05:49 -0500

    first draft of sudo code

--- main.py
+++ main.py
@@ -1,3 +1,72 @@
 #! /bin/bash/python3
 
+#ultimate goal is to have a list of players with
+#a high probability of being drafted that will 
+#produce the most
+
+### Data that needs scraped ###
+
+# 1 year stats
+
+# 2 year stats
+
+# 3 year stats
+
+# 4 year stats
+
+# 5 year stats
+
+#Skaters
+#g
+#a
+#p
+#pim
+#ppg
+#ppa
+#ppp
+#sog
+#fw
+#hit
+#blk
+
+#Goalies
+#w
+#sv
+#sa
+#sv%
+#sho
+
+### Data that needs ranked and scored individually ###
+
+#5 year stats, 0 - 100
+
+#trends, 0 - 100
+
+#prediction (takes into account trades etc. use NHL) 0 - 100
+
+#draftability (based on mock draft) 0 - 100
+
+
+### Compile all data and give overall score ###
+
+#create a scoring system,
+# 0 = worst
+# 100 = best
+
+### Create drafting strategy ###
+
+#few good players at position
+
+#roster slots
+#C = 4
+#LW = 4
+#RW = 4
+#D = 6
+#G = 2
+
+### Generate draft ranked draft list ###
+
+### Export into easy to read doc ###
+
+
 
