ad8609c0f74518d7c922a61f06036237b11b48c1
robbiedv <robbie.c.devries@gmail.com>
2019-09-08 10:34:08 -0500

    initial data scrape

--- main.py
+++ main.py
@@ -1,20 +1,41 @@
 #! /bin/bash/python3
 
 import requests 
-from requests import get
-from requests.exceptions import RequestException
-from contextlib import closing
 from bs4 import BeautifulSoup
-
-stats = requests.get('http://www.nhl.com/stats/player?aggregate=1&reportType=season&seasonFrom=20142015&seasonTo=20182019&gameType=2&filter=gamesPlayed,gte,1&sort=points,goals,assists')
-
-print(stats.text)
 
 #ultimate goal is to have a list of players with
 #a high probability of being drafted that will 
 #produce the most
 
 ### Data that needs scraped ###
+
+#skater_stats = 'http://www.nhl.com/stats/player?aggregate=1&reportType=season&seasonFrom=20142015&seasonTo=20182019&gameType=2&filter=gamesPlayed,gte,1&sort=points,goals,assists'
+#response = requests.get(skater_stats)
+#content = BeautifulSoup(response.content, "html.parser")
+
+#test = content.find_all(attrs={"class": "rt-header-cell"})
+
+#print(test.get_text())
+
+url = "https://www.hockey-reference.com/leagues/NHL_2019_skaters.html"
+
+response = requests.get(url)
+content = BeautifulSoup(response.content, "html.parser")
+
+player_stats = [ ]
+
+stats_to_scrape = ["player", "pos", "games_played", "goals", "assists", "points", "pen_min", "goals_pp", "assists_pp", "shots", "blocks", "faceoff_wins", "hits"]
+
+for x in stats_to_scrape:
+	stat = content.find("td", {"data-stat": x})
+	player_stats.append(stat.text)
+
+
+def name():
+	stat = content.find("td", {"data-stat": "player"})
+	player_stats.append(stat.text)
+
+print(player_stats)
 
 # 1 year stats
 
@@ -27,24 +48,11 @@ print(stats.text)
 # 5 year stats
 
 #Skaters
-#g
-#a
-#p
-#pim
-#ppg
-#ppa
-#ppp
-#sog
-#fw
-#hit
-#blk
+#g, a, p, pim, ppg, ppa, ppp, sog, fw, hit, blk
+
 
 #Goalies
-#w
-#sv
-#sa
-#sv%
-#sho
+#w, sv, sa, sv%, sho
 
 ### Data that needs ranked and scored individually ###
 
@@ -54,7 +62,7 @@ print(stats.text)
 
 #prediction (takes into account trades etc. use NHL) 0 - 100
 
-#draftability (based on mock draft) 0 - 100
+#draftability (based on mock drafts) 0 - 100
 
 
 ### Compile all data and give overall score ###
