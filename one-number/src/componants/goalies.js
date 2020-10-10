6c8ad7861bf1d7f59a9c5bee07c7e6d980348889
robbiedv <robbie.c.devries@gmail.com>
2020-10-10 17:26:59 -0500

    goalies display on button click

--- one-number/src/componants/goalies.js
+++ one-number/src/componants/goalies.js
@@ -4,122 +4,84 @@ import skatersJSON from "./../data/skaters.json";
 import goaliesJSON from "./../data/goalies.json";
 
 function Goalies() {
+  /*******************
+   *** SORTING DATA ***
+   *******************/
+  let goalies = [];
 
-    /*******************
-     *** SORTING DATA ***
-     *******************/
-    let rightW = [];
-    let leftW = [];
-    let center = [];
-    let defense = [];
-    let goalies = [];
-
-    /***
-    Adding each players stats together and pushing
+  /***
+    Adding each goalies stats together and pushing
     to new array based on player position
     ***/
-
-    function addSkaterStats() {
-      for (let player in skatersJSON) {
-        let stats = 0;
-        stats += parseInt(skatersJSON[player].GP);
-        stats += parseInt(skatersJSON[player].G);
-        stats += parseInt(skatersJSON[player].A);
-        stats += parseInt(skatersJSON[player].P);
-        stats += parseInt(skatersJSON[player].PM * 0.5);
-        stats += parseInt(skatersJSON[player].PPG * 5);
-        stats += parseInt(skatersJSON[player].PPA * 5);
-        stats += parseInt(skatersJSON[player].S);
-        stats += parseInt(skatersJSON[player].BLK);
-        stats += parseInt(skatersJSON[player].H * 0.5);
-        stats += parseInt(skatersJSON[player].FW) * 0.05;
-        let pos = skatersJSON[player].Pos;
-        stats = Math.round(stats / 10);
-        if (pos === "RW") {
-          rightW.push([player, pos, stats]);
-        } else if (pos === "LW") {
-          leftW.push([player, pos, stats]);
-        } else if (pos === "C" || pos === "F") {
-          center.push([player, pos, stats]);
-        } else if (pos === "D") {
-          defense.push([player, pos, stats]);
-        }
-      }
+  function addGoalieStats() {
+    for (let player in goaliesJSON) {
+      let stats = 0;
+      stats += parseInt(goaliesJSON[player].GS);
+      stats += parseInt(goaliesJSON[player].W);
+      stats += parseInt(goaliesJSON[player].GA);
+      stats += parseInt(goaliesJSON[player].SV);
+      stats += parseInt(goaliesJSON[player].SV);
+      stats += parseInt(goaliesJSON[player].SH * 50);
+      let pos = goaliesJSON[player].Pos;
+      stats = Math.round(stats / 35);
+      goalies.push([player, pos, stats]);
     }
+  }
 
-    function addGoalieStats() {
-      for (let player in goaliesJSON) {
-        let stats = 0;
-        stats += parseInt(goaliesJSON[player].GS);
-        stats += parseInt(goaliesJSON[player].W);
-        stats += parseInt(goaliesJSON[player].GA);
-        stats += parseInt(goaliesJSON[player].SV);
-        stats += parseInt(goaliesJSON[player].SV);
-        stats += parseInt(goaliesJSON[player].SH * 50);
-        let pos = goaliesJSON[player].Pos;
-        stats = stats / 50;
-        goalies.push([player, pos, stats]);
-      }
-    }
-
-    /***
+  /***
     Callback function for sorting players based
     on number of total stats in descending oder
     ***/
+  let callbackArr = [1, 4];
 
-    let callbackArr = [1, 4];
-
-    function sortPlayers(a, b) {
-      for (let index in callbackArr) {
-        if (a[2] > b[2]) {
-          return -1;
-        } else if (b[2] > a[2]) {
-          return 1;
-        } else {
-          return 0;
-        }
+  function sortGoalies(a, b) {
+    for (let index in callbackArr) {
+      if (a[2] > b[2]) {
+        return -1;
+      } else if (b[2] > a[2]) {
+        return 1;
+      } else {
+        return 0;
       }
     }
+  }
 
-    let oneNumber = [];
+  function displayRankings() {
+    for (let i = 0; i < goalies.length; i++) {
+      let table = document.getElementById("goalieTable");
+      let row = table.insertRow(i);
+      let rank = row.insertCell(0);
+      let player = row.insertCell(1);
+      let num = row.insertCell(2);
 
-    function skaterSpread(position) {
-      let one = position.sort(sortPlayers)[0][2];
-      let twenty = position.sort(sortPlayers)[20][2];
-      let spread = one - twenty;
-
-      for (let i = 0; i < position.length; i++) {
-        position[i][2] += spread;
-        oneNumber.push(position[i]);
-      }
-
-      console.log(one, twenty, spread);
+      rank.innerHTML = i + 1;
+      player.innerHTML = goalies.sort(sortGoalies)[i][0];
+      num.innerHTML = goalies.sort(sortGoalies)[i][2];
     }
+  }
 
-    function allSkaterSpread() {
-      skaterSpread(leftW);
-      skaterSpread(rightW);
-      skaterSpread(center);
-      skaterSpread(defense);
-    }
-
-    addSkaterStats();
+  /****************
+   *** COMPONANT ***
+   ****************/
+  function clickHandler() {
+    let x = document.getElementById("goalieButton");
+    x.style.display = "none";
     addGoalieStats();
-    allSkaterSpread();
-
-    /****************
-     *** COMPONANT ***
-     ****************/
-    // function clickHandler() {
-    //   console.log(oneNumber.sort(sortPlayers));
-    // }
+    setTimeout(displayRankings, 2000)
+  }
   return (
     <div className="goalies">
       <div className="grid-container">
         <h1 className="main-title">Goalies</h1>
-        <button id="goalieButton" className="statButton" type="submit">
+        <button
+          id="goalieButton"
+          className="statButton"
+          type="submit"
+          onClick={clickHandler}
+        >
           Get Draft Rankings
         </button>
+        <table id="goalieTable"></table>
       </div>
     </div>
   );
--- one-number/src/componants/skaters.js
+++ one-number/src/componants/skaters.js
@@ -3,128 +3,130 @@ import "./../css/App.css";
 import skatersJSON from "./../data/skaters.json";
 
 function Skaters() {
+  /*******************
+   *** SORTING DATA ***
+   *******************/
+  let rightW = [];
+  let leftW = [];
+  let center = [];
+  let defense = [];
 
-    /*******************
-     *** SORTING DATA ***
-     *******************/
-    let rightW = [];
-    let leftW = [];
-    let center = [];
-    let defense = [];
-
-
-    /***
+  /***
     Adding each players stats together and pushing
     to new array based on player position
     ***/
 
-    function addSkaterStats() {
-      for (let player in skatersJSON) {
-        let stats = 0;
-        stats += parseInt(skatersJSON[player].GP);
-        stats += parseInt(skatersJSON[player].G);
-        stats += parseInt(skatersJSON[player].A);
-        stats += parseInt(skatersJSON[player].P);
-        stats += parseInt(skatersJSON[player].PM * 0.5);
-        stats += parseInt(skatersJSON[player].PPG * 5);
-        stats += parseInt(skatersJSON[player].PPA * 5);
-        stats += parseInt(skatersJSON[player].S);
-        stats += parseInt(skatersJSON[player].BLK);
-        stats += parseInt(skatersJSON[player].H * 0.5);
-        stats += parseInt(skatersJSON[player].FW) * 0.05;
-        let pos = skatersJSON[player].Pos;
-        stats = Math.round(stats / 10);
-        if (pos === "RW") {
-          rightW.push([player, pos, stats]);
-        } else if (pos === "LW") {
-          leftW.push([player, pos, stats]);
-        } else if (pos === "C" || pos === "F") {
-          center.push([player, pos, stats]);
-        } else if (pos === "D") {
-          defense.push([player, pos, stats]);
-        }
+  function addSkaterStats() {
+    for (let player in skatersJSON) {
+      let stats = 0;
+      stats += parseInt(skatersJSON[player].GP);
+      stats += parseInt(skatersJSON[player].G);
+      stats += parseInt(skatersJSON[player].A);
+      stats += parseInt(skatersJSON[player].P);
+      stats += parseInt(skatersJSON[player].PM * 0.5);
+      stats += parseInt(skatersJSON[player].PPG * 5);
+      stats += parseInt(skatersJSON[player].PPA * 5);
+      stats += parseInt(skatersJSON[player].S);
+      stats += parseInt(skatersJSON[player].BLK);
+      stats += parseInt(skatersJSON[player].H * 0.5);
+      stats += parseInt(skatersJSON[player].FW) * 0.05;
+      let pos = skatersJSON[player].Pos;
+      stats = Math.round(stats / 10);
+      if (pos === "RW") {
+        rightW.push([player, pos, stats]);
+      } else if (pos === "LW") {
+        leftW.push([player, pos, stats]);
+      } else if (pos === "C" || pos === "F") {
+        center.push([player, pos, stats]);
+      } else if (pos === "D") {
+        defense.push([player, pos, stats]);
       }
     }
+  }
 
-    /***
+  /***
     Callback function for sorting players based
     on number of total stats in descending oder
     ***/
-    let callbackArr = [1, 4];
+  let callbackArr = [1, 4];
 
-    function sortPlayers(a, b) {
-      for (let index in callbackArr) {
-        if (a[2] > b[2]) {
-          return -1;
-        } else if (b[2] > a[2]) {
-          return 1;
-        } else {
-          return 0;
-        }
+  function sortPlayers(a, b) {
+    for (let index in callbackArr) {
+      if (a[2] > b[2]) {
+        return -1;
+      } else if (b[2] > a[2]) {
+        return 1;
+      } else {
+        return 0;
       }
     }
+  }
 
-    let oneNumber = [];
+  let oneNumber = [];
 
-    function skaterSpread(position) {
-      let one = position.sort(sortPlayers)[0][2];
-      let twenty = position.sort(sortPlayers)[20][2];
-      let spread = one - twenty;
+  function skaterSpread(position) {
+    let one = position.sort(sortPlayers)[0][2];
+    let twenty = position.sort(sortPlayers)[20][2];
+    let spread = one - twenty;
 
-      for (let i = 0; i < position.length; i++) {
-        position[i][2] += spread;
-        oneNumber.push(position[i]);
-      }
+    for (let i = 0; i < position.length; i++) {
+      position[i][2] += spread;
+      oneNumber.push(position[i]);
     }
+  }
 
-    function allSkaterSpread() {
-      skaterSpread(leftW);
-      skaterSpread(rightW);
-      skaterSpread(center);
-      skaterSpread(defense);
+  function allSkaterSpread() {
+    skaterSpread(leftW);
+    skaterSpread(rightW);
+    skaterSpread(center);
+    skaterSpread(defense);
+  }
+
+  /***********************
+   *** DISPLAY RANKINGS ***
+   ***********************/
+
+  function displayRankings() {
+    for (let i = 0; i < 500; i++) {
+      let table = document.getElementById("skaterTable");
+      let row = table.insertRow(i);
+      let rank = row.insertCell(0);
+      let player = row.insertCell(1);
+      let pos = row.insertCell(2);
+      let num = row.insertCell(3);
+
+      rank.innerHTML = i + 1;
+      player.innerHTML = oneNumber.sort(sortPlayers)[i][0];
+      pos.innerHTML = oneNumber.sort(sortPlayers)[i][1];
+      num.innerHTML = oneNumber.sort(sortPlayers)[i][2];
     }
+  }
 
-    /***********************
-    *** DISPLAY RANKINGS ***
-    ***********************/
+  /****************
+   *** COMPONANT ***
+   ****************/
 
-    function displayRankings() {
-      //for top 500 players
-      for(let i = 0; i < 500; i++) {
+  function clickHandler() {
+    let x = document.getElementById("skaterButton");
+    x.style.display = "none";
+    addSkaterStats();
+    allSkaterSpread();
+    setTimeout(displayRankings, 10)
+  }
 
-        let table = document.getElementById("statTable");
-        let row = table.insertRow(i);
-        let player = row.insertCell(0);
-        let pos = row.insertCell(1);
-        let num = row.insertCell(2);
-
-        player.innerHTML = oneNumber.sort(sortPlayers)[i][0]
-        pos.innerHTML = oneNumber.sort(sortPlayers)[i][1];
-        num.innerHTML = oneNumber.sort(sortPlayers)[i][2];
-      }
-      //create a new row with 3 columns
-      //insert data into rows
-      //insert into compnant
-    }
-
-
-    /****************
-    *** COMPONANT ***
-    ****************/
-    function clickHandler() {
-      addSkaterStats();
-      allSkaterSpread();
-      displayRankings();
-      console.log(oneNumber.sort(sortPlayers));
-    }
   return (
     <div className="skaters">
       <div className="grid-container">
         <h1 className="main-title">Skaters</h1>
-        <button id="skaterButton" className="statButton" type="submit" onClick={clickHandler}>
+        <button
+          id="skaterButton"
+          className="statButton"
+          type="submit"
+          onClick={clickHandler}
+        >
           Get Draft Rankings
         </button>
-        <table id="statTable"></table>
+        <table id="skaterTable"></table>
       </div>
     </div>
   );
